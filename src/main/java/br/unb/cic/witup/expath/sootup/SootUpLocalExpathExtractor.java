f188107c1dee008b973bf55a2a15a1b80c33693c
Marcelo Amorim <58489782+marcelomamorim@users.noreply.github.com>
2026-01-22 07:47:16 -0300

    fix(expath): align local extractor with sootup api

--- src/main/java/br/unb/cic/witup/expath/sootup/SootUpLocalExpathExtractor.java
+++ src/main/java/br/unb/cic/witup/expath/sootup/SootUpLocalExpathExtractor.java
@@ -3,11 +3,8 @@ package br.unb.cic.witup.expath.sootup;
 import br.unb.cic.witup.expath.LocalExpath;
 import br.unb.cic.witup.expath.LocalExpathExtractor;
 import br.unb.cic.witup.expath.condition.AtomicCondition;
-import br.unb.cic.witup.expath.condition.NotCondition;
-import br.unb.cic.witup.expath.condition.OrCondition;
 import br.unb.cic.witup.expath.condition.PathCondition;
 import br.unb.cic.witup.expath.condition.TrueCondition;
-import sootup.core.graph.StmtGraph;
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.jimple.common.stmt.JIfStmt;
 import sootup.core.jimple.common.stmt.JThrowStmt;
@@ -59,26 +56,14 @@ public final class SootUpLocalExpathExtractor implements LocalExpathExtractor {
 
     private List<LocalExpath> extractFromMethod(final JavaSootMethod sootMethod) {
         Body body = sootMethod.getBody();
-        StmtGraph<?> graph = body.getStmtGraph();
         List<Stmt> orderedStatements = body.getStmts();
         List<LocalExpath> expaths = new ArrayList<>();
 
-        for (Stmt stmt : graph) {
+        for (Stmt stmt : orderedStatements) {
             if (!(stmt instanceof JThrowStmt throwStmt)) {
                 continue;
             }
-            List<PathCondition> incomingConditions = new ArrayList<>();
-            for (Stmt pred : graph.getPredsOf(stmt)) {
-                if (pred instanceof JIfStmt ifStmt) {
-                    PathCondition condition = new AtomicCondition(ifStmt.getCondition().toString());
-                    if (!isThrowOnTrueBranch(ifStmt, throwStmt, orderedStatements)) {
-                        condition = new NotCondition(condition);
-                    }
-                    incomingConditions.add(condition);
-                }
-            }
-
-            PathCondition pathCondition = buildPathCondition(incomingConditions);
+            PathCondition pathCondition = findConditionForThrow(orderedStatements, throwStmt);
             List<Stmt> pathNodes = buildPathNodes(orderedStatements, throwStmt);
             String exceptionType = throwStmt.getOp().getType().toString();
 
@@ -94,26 +79,15 @@ public final class SootUpLocalExpathExtractor implements LocalExpathExtractor {
         return expaths;
     }
 
-    private boolean isThrowOnTrueBranch(final JIfStmt ifStmt, final JThrowStmt throwStmt, final List<Stmt> orderedStatements) {
-        Stmt target = ifStmt.getTarget();
-        if (target.equals(throwStmt)) {
-            return true;
+    private PathCondition findConditionForThrow(final List<Stmt> orderedStatements, final JThrowStmt throwStmt) {
+        int throwIndex = orderedStatements.indexOf(throwStmt);
+        for (int index = throwIndex - 1; index >= 0; index--) {
+            Stmt candidate = orderedStatements.get(index);
+            if (candidate instanceof JIfStmt ifStmt) {
+                return new AtomicCondition(ifStmt.getCondition().toString());
+            }
         }
-        int ifIndex = orderedStatements.indexOf(ifStmt);
-        if (ifIndex >= 0 && ifIndex + 1 < orderedStatements.size()) {
-            return orderedStatements.get(ifIndex + 1).equals(throwStmt);
-        }
-        return false;
-    }
-
-    private PathCondition buildPathCondition(final List<PathCondition> incomingConditions) {
-        if (incomingConditions.isEmpty()) {
-            return new TrueCondition();
-        }
-        if (incomingConditions.size() == 1) {
-            return incomingConditions.get(0);
-        }
-        return new OrCondition(incomingConditions);
+        return new TrueCondition();
     }
 
     private List<Stmt> buildPathNodes(final List<Stmt> orderedStatements, final JThrowStmt throwStmt) {
